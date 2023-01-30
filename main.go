d13125be9b76a1e63ca9ffedd3ff58a5f27b05b4
Nohely Badillo <nohely.badillo96@gmail.com>
2023-01-30 02:21:52 -0600

    experimenting a little with constants and variables :bu

--- main.go
+++ main.go
@@ -3,5 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Declarando constantes
+	const pi float64 = 3.14159265359
+	fmt.Println("Our value of pi is:", pi)
+
+	// Declarando variables enteras
+	// Go no compila si las variables no son usadas
+	base := 12
+	var altura int = 14
+	var area int // aqui go agrega 0 como valor de default
+
+	fmt.Println("values:", base, altura, area)
+
+	// Zero values (valores por default)
+	var a int
+	var b float64
+	var c string
+	var d bool
+
+	fmt.Println("Wow! look at this pretty zeros:", a, b, c, d)
 }
